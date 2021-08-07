31f261dab6437f76019c6409663732ca1f626bf0
Shikha Singh <32593425+SHIKHASINGH1506@users.noreply.github.com>
2021-08-07 21:00:04 +0530

    selected all the required elements using QuerySelector

---
+++ app.js
@@ -0,0 +1,5 @@
+const angles = document.querySelectorAll(".angles");
+const checkSumOfAngle = document.querySelector("#check-sum-of-angle");
+const outputArea = document.querySelector("#output-area");
+
+
