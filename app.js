3db46dfeeb25362c2e1c982bca9a8e6a0997426d
Shikha Singh <32593425+SHIKHASINGH1506@users.noreply.github.com>
2021-08-08 13:59:46 +0530

    small modification

--- app.js
+++ app.js
@@ -1,5 +1,25 @@
-const angles = document.querySelectorAll(".angles");
-const checkSumOfAngle = document.querySelector("#check-sum-of-angle");
+const angleInput = document.querySelectorAll(".angles");
+const checkSumOfAngles = document.querySelector("#check-sum-of-angle");
 const outputArea = document.querySelector("#output-area");
+
+checkSumOfAngles.addEventListener("click", isTriangle);
+
+function isTriangle(){
+    var sum = caculateSumofAngles(Number(angleInput[0].value), Number(angleInput[1].value), Number(angleInput[2].value));
+    if(sum===180){
+        showMessage("These angles are good to form a triangle");
+    }
+    else{
+        showMessage("Sorry! Thess angles can't form a triangle")
+    }
+}
+
+function caculateSumofAngles(angle1, angle2, angle3){
+    return (angle1 + angle2 + angle3);
+}
+
+function showMessage(message){
+    outputArea.innerText = message;
+}
 
 
