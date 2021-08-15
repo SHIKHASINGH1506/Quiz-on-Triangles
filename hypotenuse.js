feb68780d53694f5e0022f3ff6b344b95a41e3f9
Shikha Singh <32593425+SHIKHASINGH1506@users.noreply.github.com>
2021-08-15 12:16:58 +0530

    calculate hypotenuse fucntionality completed

---
+++ hypotenuse.js
@@ -0,0 +1,16 @@
+const hypotenuseForm = document.querySelector("#check-hypo");
+const sides = document.querySelectorAll(".sides");
+console.log(hypotenuseForm);
+const submitBtn = document.querySelector("#submit-Btn");
+const outputArea = document.querySelector(".hypShowHere");
+
+hypotenuseForm.addEventListener('submit', calculateHypotenuse);
+
+function calculateHypotenuse(e){
+    e.preventDefault();
+   // console.log(hypotenuseForm);
+   let sideOne = Number(sides[0].value);
+   let sideTwo = Number(sides[1].value)
+   let sideThree = Math.sqrt((sideOne * sideOne) + ( sideTwo * sideTwo));
+   outputArea.innerText = sideThree.toFixed(2);
+    }
